80edeec35a2e454865e6ed232019aad3987effcc
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2020-09-07 12:20:47 +0200

    Improve deployment script to read static Matic Network config

--- migrations/02_deploy_link_token.js
+++ migrations/02_deploy_link_token.js
@@ -1,6 +1,23 @@
+const axios = require("axios");
 const LinkToken = artifacts.require("LinkTokenOnMatic");
 
-module.exports = function (deployer, network, accounts) {
-  const childChainManager = accounts[0];
-  deployer.deploy(LinkToken, childChainManager);
+const MATIC_NETWORK_STATIC_VERSION = "v2.4.3";
+const MATIC_NETWORK_NETWORK = "testnet/mumbai";
+
+const getConfigURL = (version, network) =>
+  `https://raw.githubusercontent.com/maticnetwork/static/${version}/network/${network}/index.json`;
+
+module.exports = async (deployer, network, accounts) => {
+  const configURL = getConfigURL(
+    process.env.MATIC_NETWORK_STATIC_VERSION || MATIC_NETWORK_STATIC_VERSION,
+    process.env.MATIC_NETWORK_NETWORK || MATIC_NETWORK_NETWORK
+  );
+
+  console.log(`Reading Matic Network config: `, configURL);
+  const resp = await axios.get(configURL);
+  const config = resp.data;
+
+  const childChainManager = config.Matic.POSContracts.ChildChainManagerProxy;
+  console.log(`Setting 'childChainManager': `, childChainManager);
+  await deployer.deploy(LinkToken, childChainManager);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   networks: {
-    ganache: {
+    development: {
       host: "127.0.0.1",
-      port: 7545,
+      port: 8545,
       network_id: "*",
     },
   },
