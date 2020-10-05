4407053e33400f5d691b5ddf00e78b9774bc02ca
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2020-10-05 14:40:05 +0200

    Fix legacy LINK BEP20 migration contract

--- contracts/custom/bsc/Manager0xF8A0.sol
+++ contracts/custom/bsc/Manager0xF8A0.sol
@@ -3,13 +3,16 @@ pragma solidity ^0.6.0;
 import {MigrationManager} from "./MigrationManager.sol";
 
 contract Manager0xF8A0 is MigrationManager {
-
   address public constant PREV_TOKEN = 0xF8A0BF9cF54Bb92F17374d9e9A321E6a111a51bD;
 
-  constructor(address childTokenAddr)
-    public
-    MigrationManager(PREV_TOKEN, childTokenAddr)
-  {
-    // noop
+  constructor(address childTokenAddr) public MigrationManager(PREV_TOKEN, childTokenAddr) {}
+
+  /**
+   * @dev Legacy LinkToken BEP20 deployed at 0xF8A0 does not have the `burnFrom` method,
+   * so token burning is a two step process for the migration contract.
+   */
+  function _burn(address account, uint256 amount) internal override virtual {
+    prevToken.transferFrom(account, address(this), amount);
+    prevToken.burn(amount);
   }
 }
--- contracts/custom/bsc/MigrationManager.sol
+++ contracts/custom/bsc/MigrationManager.sol
@@ -1,26 +1,28 @@
 pragma solidity ^0.6.0;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 import {IChildERC20} from "../../interfaces/IChildERC20.sol";
 
 contract MigrationManager {
-  using SafeERC20 for IERC20;
-
-  address public constant BURN_ADDRESS = address(0x0);
-
-  IERC20 public immutable prevToken;
+  ERC20Burnable public immutable prevToken;
   IChildERC20 public immutable childToken;
 
   constructor(address prevTokenAddr, address childTokenAddr) public {
-    prevToken = IERC20(prevTokenAddr);
+    prevToken = ERC20Burnable(prevTokenAddr);
     childToken = IChildERC20(childTokenAddr);
   }
 
-  // TODO: check if transfer to BURN_ADDRESS (0x0) is allowed for prevToken
   function migrate(uint256 prevTokenAmount) external {
-    prevToken.safeTransferFrom(msg.sender, BURN_ADDRESS, prevTokenAmount);
-    bytes memory depositData = abi.encode(prevTokenAmount);
-    childToken.deposit(msg.sender, depositData);
+    _burn(msg.sender, prevTokenAmount);
+    _deposit(msg.sender, prevTokenAmount);
+  }
+
+  function _burn(address account, uint256 amount) internal virtual {
+    prevToken.burnFrom(account, amount);
+  }
+
+  function _deposit(address user, uint256 amount) internal virtual {
+    bytes memory depositData = abi.encode(amount);
+    childToken.deposit(user, depositData);
   }
 }
